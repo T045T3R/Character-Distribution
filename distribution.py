012a6e3f4718dd6d14e93433d32d7f2fb0f4ecb9
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-27 13:55:34 -0400

    Updated from Brython Server: 9/27/2016 1:55:35 PM

--- distribution.py
+++ distribution.py
@@ -42,7 +42,7 @@ def compare(a, b):
     """
     compare - generic comparison function for testing two elements.
     """
-    return b >= a
+    return b <= a
 
 def bsort(seq, cmp):
     """
@@ -91,5 +91,6 @@ ycount=str(letters).count('y')
 zcount=str(letters).count('z')
 
 tosort = [acount, bcount, ccount,dcount,ecount,fcount, gcount,hcount,icount,jcount,kcount,lcount,mcount,ncount,ocount,pcount, qcount,rcount,scount,tcount,ucount,vcount,wcount,xcount,ycount,zcount]
+zip(tosort,[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
 bsort(tosort, compare)
 print(tosort)
