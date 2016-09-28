0e7b5eaeb348ec7dea318281994b05c2e5a54ef9
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-09-28 12:59:28 -0400

    Updated from Brython Server: 9/28/2016 12:59:31 PM

--- distribution.py
+++ distribution.py
@@ -36,8 +36,8 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-letters = (input("Please enter a string of text (the bigger the better): ").lower())
-
+letters = ("Please enter a string of text (the bigger the better): ")
+letters = letters.lower()
 def compare(a, b):
     """
     compare - generic comparison function for testing two elements.
