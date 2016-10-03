936eab8e947c7a61bb23d3451b9f0806092bf900
T045T3R <johannestestorf@hanovernorwichschools.org>
2016-10-03 16:42:02 -0400

    Updated from Brython Server: 10/3/2016 4:42:02 PM

--- distribution.py
+++ distribution.py
@@ -37,6 +37,7 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 letters = input("Please enter a string of text (the bigger the better): ")
+print('The distribution of characters in "'+astring+'" is: ')
 letters = letters.lower()
 def compare(a, b):
     """
